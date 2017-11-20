c131586595885d0f1e7a7b88b79a6e2c28541eaa
rgalbiati <raina.galbiati@tufts.edu>
2017-11-20 17:31:28 -0500

    deleted unused commented code and print statments

--- MapWithMotion.py
+++ MapWithMotion.py
@@ -40,7 +40,6 @@ def graphMaxVals (img, mask, color, max_val_indices) :
         for index in max_val_indices :
                 cv2.circle(img, (index[1], index[0]), 15, color, 2)
                 cv2.circle(mask, (index[1], index[0]), 5, whiteColor, -1)
-                print (index)
         return img, mask
 
 def findIntensePixels(gray, combine, img, mask):
@@ -52,31 +51,12 @@ def findIntensePixels(gray, combine, img, mask):
         max_indices = findMaxVals (combine, size)
         img, mask = graphMaxVals(img, mask, hsvColor, max_indices)
 
-        # np.set_printoptions(threshold=np.inf)
-        # print (combine)
-        # sys.exit()
-
-
-        # for itter in range (100) :
-        #         (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
-        #         cv2.circle(mask, maxLoc, 5, (0, 255, 0), -1)
-        #         cv2.circle(gray, maxLoc, 5, (0, 0, 0), -1)
-        #         cv2.circle(img, maxLoc, 10, brightnessColor, 2)
-
-        #         (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(combine)
-        #         cv2.circle(mask, maxLoc, 5, (255, 255, 255), -1)
-        #         cv2.circle(combine, maxLoc, 5, (0, 0, 0), -1)
-        #         cv2.circle(img, maxLoc, 10, hsvColor, 2)
         return img, mask
 
 # Purpose: returns an array with combined value and saturation values
 def getSaturationArray (img) :
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         blur = cv2.GaussianBlur(hsv, (41, 41), 0)
-        # half = np.full(hsv.shape[:2], 2)
-        # combine = np.add(np.divide(blur[:, :, 1], half), 
-        #         np.divide(blur[:, :, 2], half))
-        # return combine
         return blur[:, :, 1]
 
 # Purpose: returns grayscale version of image
@@ -88,45 +68,15 @@ def getGrayImg (img) :
         return gray
 
 # ----------------------------- Camera Functions ----------------------------- #
-# Purpose: gets initial values from video feed 
-def initImage () :
-        global cam
-        t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-        s, img = cam.read()
-        t = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-        return img, s, t_minus, t, t_plus
-        # global cam
-        # t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-        # t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-        # s, img = cam.read()
-        # t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # return img, s, t_minus, t, t_plus
-
 # Purpose: gets new image and updates previous and next image for movement elem
-def getImage (t_minus, t, t_plus) :
+def getImage () :
         global cam
+       
         t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
         s, img = cam.read()
         t = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
         return img, s, t_minus, t, t_plus
-
-
-        # global cam
-        # s, img = cam.read()
-        
-        # t_minus = t
-        # t = t_plus
-        # t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # return img, s, t_minus, t, t_plus
-        # global cam
-        # s, img = cam.read()
-        
-        # t_minus = t
-        # t = t_plus
-        # t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        # return img, s, t_minus, t, t_plus
 
 # Purpose: shows mask and image in seperate windows
 def showImages(img, mask) :
@@ -140,9 +90,9 @@ def closeWebcam () :
         print ("Goodbye")
 
 # Purpose: Processes next frame of video
-def processNextFrame(img, t_minus, t, t_plus) :
+def processNextFrame() :
         
-        img, s, t_minus, t, t_plus = getImage(t_minus, t, t_plus)
+        img, s, t_minus, t, t_plus = getImage()
         
         if not s :
                 closeWebcam()
@@ -162,17 +112,11 @@ def processNextFrame(img, t_minus, t, t_plus) :
         if cv2.waitKey(10) == 27 :
                 closeWebcam()
         else : 
-                processNextFrame(img, t_minus, t, t_plus)
+                processNextFrame()
 
 # ------------------------------ MAIN FUNCTIONS ------------------------------ #
 def main() :
-        img, s, t_minus, t, t_plus = initImage()
-
-        if s: 
-                processNextFrame(img, t_minus, t, t_plus)
-        else : 
-                sys.exit("Unable to get video feed")
-        return 0
+        processNextFrame()
 
 if __name__=="__main__" :
         main()
