8e87ad7f8981d4c21933819f88f4ed33301c6653
rgalbiati <raina.galbiati@tufts.edu>
2017-11-20 17:26:43 -0500

    hsv uses only saturation, combine is commented out

--- MapWithMotion.py
+++ MapWithMotion.py
@@ -6,6 +6,7 @@ import imutils
 import cv2
 import sys
 import heapq
+import time 
 
 maskWin = "Mask"
 videoWin = "Video"
@@ -37,18 +38,23 @@ def findMaxVals (array, size) :
 
 def graphMaxVals (img, mask, color, max_val_indices) :
         for index in max_val_indices :
-                cv2.circle(img, (index[0], index[1]), 10, color, 2)
-                cv2.circle(mask, (index[0], index[1]), 5, whiteColor, -1)
+                cv2.circle(img, (index[1], index[0]), 15, color, 2)
+                cv2.circle(mask, (index[1], index[0]), 5, whiteColor, -1)
+                print (index)
         return img, mask
 
 def findIntensePixels(gray, combine, img, mask):
         size = img.shape[:2]
-        
+
         max_indices = findMaxVals (gray, size)
         img, mask = graphMaxVals(img, mask, brightnessColor, max_indices)
 
         max_indices = findMaxVals (combine, size)
         img, mask = graphMaxVals(img, mask, hsvColor, max_indices)
+
+        # np.set_printoptions(threshold=np.inf)
+        # print (combine)
+        # sys.exit()
 
 
         # for itter in range (100) :
@@ -64,13 +70,14 @@ def findIntensePixels(gray, combine, img, mask):
         return img, mask
 
 # Purpose: returns an array with combined value and saturation values
-def getSaturationValueArray (img) :
+def getSaturationArray (img) :
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         blur = cv2.GaussianBlur(hsv, (41, 41), 0)
-        half = np.full(hsv.shape[:2], 2)
-        combine = np.add(np.divide(blur[:, :, 1], half), 
-                np.divide(blur[:, :, 2], half))
-        return combine
+        # half = np.full(hsv.shape[:2], 2)
+        # combine = np.add(np.divide(blur[:, :, 1], half), 
+        #         np.divide(blur[:, :, 2], half))
+        # return combine
+        return blur[:, :, 1]
 
 # Purpose: returns grayscale version of image
 def getGrayImg (img) :
@@ -83,21 +90,43 @@ def getGrayImg (img) :
 # ----------------------------- Camera Functions ----------------------------- #
 # Purpose: gets initial values from video feed 
 def initImage () :
+        global cam
         t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
-        t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
         s, img = cam.read()
-        t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        t = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
         return img, s, t_minus, t, t_plus
+        # global cam
+        # t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+        # t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+        # s, img = cam.read()
+        # t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # return img, s, t_minus, t, t_plus
 
 # Purpose: gets new image and updates previous and next image for movement elem
 def getImage (t_minus, t, t_plus) :
         global cam
+        t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
         s, img = cam.read()
-        
-        t_minus = t
-        t = t_plus
-        t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        t = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        t_plus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
         return img, s, t_minus, t, t_plus
+
+
+        # global cam
+        # s, img = cam.read()
+        
+        # t_minus = t
+        # t = t_plus
+        # t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # return img, s, t_minus, t, t_plus
+        # global cam
+        # s, img = cam.read()
+        
+        # t_minus = t
+        # t = t_plus
+        # t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        # return img, s, t_minus, t, t_plus
 
 # Purpose: shows mask and image in seperate windows
 def showImages(img, mask) :
@@ -112,6 +141,7 @@ def closeWebcam () :
 
 # Purpose: Processes next frame of video
 def processNextFrame(img, t_minus, t, t_plus) :
+        
         img, s, t_minus, t, t_plus = getImage(t_minus, t, t_plus)
         
         if not s :
@@ -122,7 +152,7 @@ def processNextFrame(img, t_minus, t, t_plus) :
 
         img = cv2.flip(img, 1)
 
-        combine = getSaturationValueArray(img)
+        combine = getSaturationArray(img)
         gray = getGrayImg(img)
         
         img, mask = findIntensePixels(gray, combine, img, mask)
